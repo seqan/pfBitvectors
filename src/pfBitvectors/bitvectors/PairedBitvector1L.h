bfd5a42fef6766a635f623449f338fac37ba2b17
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2025-09-12 23:17:46 +0200

    patch: remove unused code

--- src/pfBitvectors/bitvectors/PairedBitvector1L.h
+++ src/pfBitvectors/bitvectors/PairedBitvector1L.h
@@ -139,32 +139,4 @@ struct PairedBitvector1L {
     }
 };
 
-//using PairedL0_64Bitvector  = PairedBitvector1L<64>;
-//using PairedL0_128Bitvector = PairedBitvector1L<128>;
-//using PairedL0_256Bitvector = PairedBitvector1L<256>;
-//using PairedL0_512Bitvector = PairedBitvector1L<512>;
-//using PairedL0_1024Bitvector = PairedBitvector1L<1024>;
-//using PairedL0_2048Bitvector = PairedBitvector1L<2048>;
-//
-//static_assert(Bitvector_c<PairedL0_64Bitvector>);
-//static_assert(Bitvector_c<PairedL0_128Bitvector>);
-//static_assert(Bitvector_c<PairedL0_256Bitvector>);
-//static_assert(Bitvector_c<PairedL0_512Bitvector>);
-//static_assert(Bitvector_c<PairedL0_1024Bitvector>);
-//static_assert(Bitvector_c<PairedL0_2048Bitvector>);
-//
-//using PairedL0_64BitvectorUA  = PairedBitvector1L<64, false>;
-//using PairedL0_128BitvectorUA = PairedBitvector1L<128, false>;
-//using PairedL0_256BitvectorUA = PairedBitvector1L<256, false>;
-//using PairedL0_512BitvectorUA = PairedBitvector1L<512, false>;
-//using PairedL0_1024BitvectorUA = PairedBitvector1L<1024, false>;
-//using PairedL0_2048BitvectorUA = PairedBitvector1L<2048, false>;
-//
-//static_assert(Bitvector_c<PairedL0_64BitvectorUA>);
-//static_assert(Bitvector_c<PairedL0_128BitvectorUA>);
-//static_assert(Bitvector_c<PairedL0_256BitvectorUA>);
-//static_assert(Bitvector_c<PairedL0_512BitvectorUA>);
-//static_assert(Bitvector_c<PairedL0_1024BitvectorUA>);
-//static_assert(Bitvector_c<PairedL0_2048BitvectorUA>);
-
 }
